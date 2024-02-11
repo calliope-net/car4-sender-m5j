397aa82ca93089bcb19fef61323afa60d108a499
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-11 20:05:27 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,38 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    bJoy = false
+    basic.showIcon(IconNames.ArrowWest)
+})
 function fServo (pJoy: number) {
-    if (lcd16x2rgb.between(pJoy, 126, 130)) {
+    if (lcd16x2rgb.between(pJoy, 126, 134)) {
         qwiicjoystick.comment("Ruhestellung soll 512 ist 498 auf 512 = 90° anpassen")
         qwiicjoystick.comment("Ruhestellung soll 128 ist xx auf 128 = 90° anpassen")
         return 90
-    } else if (pJoy < 8) {
+    } else if (pJoy < 20) {
         qwiicjoystick.comment("Werte < 8 wie 0 behandeln (max links)")
         return 135
-    } else if (pJoy > 250) {
+    } else if (pJoy > 235) {
         qwiicjoystick.comment("Werte > 250 wie 255 behandeln (max rechts)")
         return 45
     } else {
         qwiicjoystick.comment("Werte von 32 bis 991 auf 46° bis 134° verteilen")
-        return Math.round(Math.map(pJoy, 7, 250, 134, 46))
+        return Math.round(Math.map(pJoy, 20, 235, 134, 46))
     }
 }
 function m5Joystick () {
     oBuffer = i2c.i2cReadBuffer(i2c.i2c_eADDR(i2c.eADDR.Joystick_x52), 3)
     iMotor = oBuffer.getUint8(0)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 12, 15, iMotor, lcd16x2rgb.eAlign.right)
+    if (lcd16x2rgb.between(iMotor, 120, 138)) {
+        iMotor = 127
+    }
+    iMotor = 255 - iMotor
     iServo = fServo(oBuffer.getUint8(1))
     fServoDisplay(oBuffer.getUint8(1))
 }
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    bJoy = true
+    basic.showIcon(IconNames.ArrowEast)
+})
 function fServoDisplay (pJoy: number) {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 3, pJoy, lcd16x2rgb.eAlign.right)
     if (pJoy > 0 && pJoy < xmin) {
@@ -27,7 +40,7 @@ function fServoDisplay (pJoy: number) {
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 4, 7, xmin, lcd16x2rgb.eAlign.right)
     } else if (pJoy < 255 && pJoy > xmax) {
         xmax = pJoy
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 9, 12, xmax, lcd16x2rgb.eAlign.right)
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 8, 11, xmax, lcd16x2rgb.eAlign.right)
     }
 }
 function qwiicJoystick () {
@@ -36,26 +49,31 @@ function qwiicJoystick () {
     iServo = fServo(aJoy[1])
     fServoDisplay(aJoy[1])
 }
+let aJoy: number[] = []
 let iServo = 0
 let iMotor = 0
 let oBuffer: i2c.i2cclass = null
+let bJoy = false
 let xmax = 0
 let xmin = 0
-let aJoy: number[] = []
 lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
-qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
-aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.A_0_1023)
 radio.setGroup(240)
 radio.setTransmitPower(7)
 xmin = 128
 xmax = 128
-loops.everyInterval(400, function () {
-    let iFahrstrecke = 0
-    if (iFahrstrecke == 0) {
+basic.pause(1000)
+let iFahrstrecke = 1
+loops.everyInterval(500, function () {
+    if (iFahrstrecke == 1) {
         basic.setLedColor(0x007fff)
-        qwiicJoystick()
+        if (true) {
+            m5Joystick()
+        } else {
+            qwiicJoystick()
+        }
         qwiicjoystick.comment("0 Motor 0..128..255")
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 3, iServo, lcd16x2rgb.eAlign.right)
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 12, 15, iMotor, lcd16x2rgb.eAlign.right)
         qwiicjoystick.comment("1 Servo 0..128..255 -> 45..90..135")
         qwiicjoystick.setSendeZahl(NumberFormat.Int8LE, qwiicjoystick.eOffset.z0, iMotor)
         qwiicjoystick.setSendeZahl(NumberFormat.Int8LE, qwiicjoystick.eOffset.z1, iServo)
